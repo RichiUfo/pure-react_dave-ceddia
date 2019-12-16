f5a51dd3cf425c872926be9fa6e5116d86dd376d
Richi <alexey.tokarchuk@gmail.com>
2019-12-16 14:43:27 +0200

    initial commit

---
+++ react-hello/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function HelloWorld(){
+    return (
+        <div>Hello World!</div>
+    );
+}
+
+ReactDom.render(
+    <HelloWorld/>,
+    document.querySelector('#root')
+);
